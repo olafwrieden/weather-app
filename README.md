157a884dc4d0e5eb28f0bf3e78e4d7ec6609f4f5
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2020-03-16 16:18:12 +1300

    Added Initial Code

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Weather App
+A basic Node.js weather app to fetch the temperature of any city in the world using the [OpenWeather](https://openweathermap.org/) API.
+
+The source code is adapted from [this repo](https://github.com/bmorelli25/simple-nodejs-weather-app) for the purpose of creating and documenting a development environment around an existing project.
+
+### Getting Started
+
+1. Create a `.env` file in the root directory of this repo.
+2. Copy the contents of the `.env.example` file into the previously created `.env` file. Then set the variables as required. Example: `PORT=3000`
+3. Install all required dependencies by running: `npm install`
+
+### In Development
+
+Wouldn't it be nice if the page were to refresh for instant feedbacl whenever you have saved a code change? Well, that's where a package called `nodemon` comes in. In *development* , start the application using:
+
+```javascript
+$ npm run dev
+// Now open your browser and visit: localhost:3000 (or port number defined in .env)
+```
+
---
+++ server.js
@@ -0,0 +1,69 @@
+require('dotenv-safe').config();
+const request = require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const apiKey = process.env.API_KEY;
+const weather = {
+  weather: null,
+  error: null
+};
+
+let timestamp;
+
+const requestWeather = city => {
+  return new Promise((resolve, reject) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    request(url, (err, response, body) => {
+      const weatherResponse = JSON.parse(body);
+
+      if (err) {
+        let errMsg = JSON.stringify(err);
+        weather.error = "Error, please try again.";
+        console.log(`${timestamp} - ${city} - error: ${errMsg}`);
+        resolve();
+      } else if (!weatherResponse.name || !weatherResponse.main.temp) {
+        weather.error = "Error, please try again.";
+        console.log(`${timestamp} - ${city} - unexpected api output: ${body}`);
+        resolve();
+      } else {
+        weather.weather = `It's ${weatherResponse.main.temp} degrees in ${city}.`;
+        weather.error = null;
+        console.log(`${timestamp} - ${city} - ${body}`);
+        resolve();
+      }
+    });
+  });
+};
+
+const clearWeather = () => {
+  weather.weather = null;
+  weather.error = null;
+};
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", weather);
+});
+
+app.post("/", (req, res) => {
+  const city = req.body.city;
+  timestamp = Date.now();
+  console.log(`${timestamp} - ${city} - weather requested`);
+  requestWeather(city).then(() => {
+    res.render("index", weather);
+    clearWeather();
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('--------------------------');
+  console.log(`Weather App: on port ${process.env.PORT}!`);
+  console.log('--------------------------');
+});
