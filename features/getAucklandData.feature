6d5f839848c9162f368838785344ff5d65d0701a
KJ Millar <kjmillar.webdesigner@gmail.com>
2020-03-24 14:49:50 +1300

    flesh out tests

--- features/getAucklandData.feature
+++ features/getAucklandData.feature
@@ -5,3 +5,9 @@ Feature: What's the weather?
     Given my city is Auckland
     When I ask for the weather
     Then I should be told the weather
+    And the weather should contain temperature and timezone
+
+  Scenario: AEIOU is not a city
+    Given my city is AEIOU
+    When I ask for the weather
+    Then I will be told about the error
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,27 +2,46 @@ const chai = require("chai");
 const expect = chai.expect;
 const server = require("../../server").server;
 const getWeather = require("../../server").getWeather;
-const { Given, When, Then, After } = require('cucumber');
+const { Given, When, Then, After } = require("cucumber");
 
 // TEST
-Given('my city is Auckland', function () {
-    this.city = 'Auckland';
+Given("my city is Auckland", function () {
+  this.city = "Auckland";
 });
 
-When('I ask for the weather', async function () {
+When("I ask for the weather", async function () {
     this.actualAnswer = JSON.parse(await getWeather(this.city));
 });
 
-Then('I should be told the weather', function () {
+Then("I should be told the weather", function () {
     expect(this.actualAnswer).to.be.an("object");
     expect(this.actualAnswer).to.not.be.undefined;
-    expect(this.actualAnswer)
-      .to.have.property("main")
-      .to.be.an("object");
     expect(this.actualAnswer)
       .to.have.property("weather")
       .to.be.an("array")
       .of.length(1);
+});
+
+Then('the weather should contain temperature and timezone', function () {
+  expect(this.actualAnswer)
+  expect(this.actualAnswer)
+    .to.have.property("main")
+    .to.be.an("object")
+    .to.have.property("temp")
+    .to.be.a("number")
+  expect(this.actualAnswer)
+    .to.have.property("timezone")
+    .to.be.a("number")
+});
+
+Given("my city is AEIOU", function () {
+  this.city = "AEIOU";
+});
+
+Then('I will be told about the error', function () {
+  expect(this.actualAnswer)
+    .to.be.a("string")
+    .to.equal("Data error, please try again.")
 });
 
 // STOP API SERVER
